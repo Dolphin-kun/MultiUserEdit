04d34f8a9d2910301295fe725ec4f9154edb6a15
Dolphin-kun <dolphin.discord.js@gmail.com>
2026-09-21 21:11:17 +0900

    PluginDetailsを追加

--- MultiUserEdit/MultiUserEditPlugin.cs
+++ MultiUserEdit/MultiUserEditPlugin.cs
@@ -1,9 +1,11 @@
 ﻿using MultiUserEdit.ViewModels;
 using MultiUserEdit.Views;
+using System.Reflection;
 using YukkuriMovieMaker.Plugin;
 
 namespace MultiUserEdit
 {
+    [PluginDetails(AuthorName ="いるかぁぁ",ContentId = "nc505669")]
     public class MultiUserEditPlugin : IToolPlugin
     {
         public const string PluginName = "共同編集";
@@ -11,5 +13,7 @@ namespace MultiUserEdit
         public string Name => PluginName;
         public Type ViewModelType => typeof(MultiUserEditViewModel);
         public Type ViewType => typeof(MultiUserEditView);
+
+        public PluginDetailsAttribute Details => GetType().GetCustomAttribute<PluginDetailsAttribute>() ?? new();
     }
 }
